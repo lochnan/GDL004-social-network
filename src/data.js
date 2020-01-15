4e774ca480f09e41bc574ebf80d2dacc9cd6fa5e
lochnan <56927193+lochnan@users.noreply.github.com>
2020-01-14 22:16:56 -0600

    Nancy14Enero SignIn

--- src/data.js
+++ src/data.js
@@ -1,3 +1,18 @@
-import { example } from './example.js';
+export const example = () => {
+    // aquí tu código
+}
 
-example();
+// Function that triggers with the SignUp button with the Firestore Auth
+export function toSignUp () {
+    let email =  document.getElementById('email').value;
+    let passwordEmail = document.getElementById('passwordEmail').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, passwordEmail).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("Te equivocaste");
+      console.trace(error);
+      // ...
+    });
+  }
