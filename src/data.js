8249f6323a83be316671c2ddda8dba30c2f8a082
lochnan <56927193+lochnan@users.noreply.github.com>
2020-01-19 13:06:44 -0600

    Domingo Nancy

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
-
-// FUNCTION that calls Firebase 
-firebase.auth().onAuthStateChanged(function(user) {
+// FUNCTION that calls Firebase
+firebase.auth().onAuthStateChanged((user) => {
   console.log(user);
   if (user) {
     // User is signed in.
@@ -11,48 +10,45 @@ firebase.auth().onAuthStateChanged(function(user) {
     let isAnonymous = user.isAnonymous;
     let uid = user.uid;
     let providerData = user.providerData;
-    document.getElementById("prueba").innerHTML= "Puedes iniciar sesión";
-    
+    document.getElementById('prueba').innerHTML = 'Puedes iniciar sesión';
   } else {
-    document.getElementById("prueba").innerHTML= " ";
+    document.getElementById('prueba').innerHTML = '';
     // User is signed out.
   }
 });
 
 // FUNCTION that triggers with the SignUp button with the Firestore Auth but also closes de modal
-export function toSignUp () {
-  let email =  document.getElementById('email').value;
-    let passwordEmail = document.getElementById('passwordEmail').value;
+export function toSignUp() {
+  let email = document.getElementById('email').value;
+  let passwordEmail = document.getElementById('passwordEmail').value;
 
-    console.log(email, passwordEmail);
-
-  firebase.auth().createUserWithEmailAndPassword(email, passwordEmail).catch(function(error) {
-    // Handle Errors here.
-    let errorCode = error.code;
+  firebase.auth().createUserWithEmailAndPassword(email, passwordEmail).catch((error) => {
+  // Handle Errors here.
+  let errorCode = error.code;
     let errorMessage = error.message;
-    //alert("Te equivocaste");
+    // alert("Te equivocaste")
     console.trace(error);
     alert(error.message);
     });
-  
+
   // When the user clicks on the button, close de modal and returns to the Login page
-  document.getElementById('mymodal').style.display = "none";
-  document.getElementById('signUp').style.display = "none";
-  }
+  document.getElementById('mymodal').style.display = 'none';
+  document.getElementById('signUp').style.display = 'none';
+}
 // IGNOREN ESTO ATTE. NANCY import hola kfkfes.js
 
 // FUNCTION that makes login once the user has already signup with the email.
-export function logIn () {
+export function logIn() {
   const emailL = document.getElementById('emailL').value;
   const passwordL = document.getElementById('passwordL').value;
 
- // IGNOREN ESTO ATTE. NANCY hola (emailL, passwordL); 
-// IGNOREN ESTO ATTE. NANCY export function hola (emailL, passwordL) {firebase.auth().signInWithEmailAndPassword(emailL, passwordL).then(function(){
+/* IGNOREN ESTO ATTE. NANCY hola (emailL, passwordL);
+export function hola (emailL, passwordL) {firebase.auth().signInWithEmailAndPassword
+  (emailL, passwordL).then(function(){ */
 
   firebase.auth().signInWithEmailAndPassword(emailL, passwordL).catch((error) => {
     let errorCode = error.code;
     let errorMessage = error.message;
-    alert(errorMessage);
   });
 }
 
